59c37c0eab557f7be41cd49a5584e684de56ef7e
Varungowda123 <127393826+Varungowda123@users.noreply.github.com>
2023-07-19 21:56:09 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,29 @@
+import java.util.*; 
+public class Demo {
+    public static final  void main(String[] args) {
+        String[] nums= new String[]{"1","19","12"};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+        
+//     String s1=new String("bob");
+//         String s2=new String("good");
+//         String s3="bob";
+//         String s4="good";
+//         String s5=s3+s2;
+//         String s6=s1+s4;
+
+//         System.out.print(s5==s6);
+   
+        // String notbob=  bob;
+    // System.out.println((bob==notbob)+" "+(bob.equals(notbob)));
+    }
+         
+        // String  s1 = new String("java");
+        // String  s2 = "java";
+        // System.out.println((s1==s2)+" "+(s1.equals(s2)));
+        // // System.out.println);
+
+    
+}
+
